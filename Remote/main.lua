8c31dde090ad38492903c9ed8c079b72ad4a9a56
Ruscios <toddjr@bertels.net>
2020-04-17 09:27:14 -0700

    Create main.lua with initial declarations

---
+++ Remote/main.lua
@@ -0,0 +1,10 @@
+local event = require("event")
+local component = require("component")
+local modem = component.modem
+local core = component.draconic_rf_storage
+local reactor = component.reactor_logic_adapter
+local keyboard = require("keyboard")
+
+while true do
+
+end
